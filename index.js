c9e270d276d478a4d0431fb27c24b64cf0192b9d
Marvin <marvinepoquiz@gmail.com>
2021-11-26 14:02:29 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(books=>renderBooks(books))
 }
+function initialize(){
+  fetchBooks()
+}
+initialize()
+	
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +21,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
